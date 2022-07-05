ea622db5036360c027c19a51bd302f1cd70a61b1
aman.saxena <aman.saxena@bfhl.in>
2022-07-05 23:05:05 +0530

    leetcode-560,532

--- Array/Array.js
+++ Array/Array.js
@@ -40,4 +40,42 @@ array.push(13); //pushing element
 array.push(14);
 array.push(10);
 array.push(989);
-array.pop(); //Popping element 989 
+array.pop(); //Popping element 989
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const nums = [5, 5, 5, 5];
+const k = 10;
+var subarraySum = function () {
+  const map = {};
+  let ans = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map[Number(nums[i])]) {
+      map[Number(nums[i])] = map[Number(nums[i])] + 1;
+    } else {
+      map[Number(nums[i])] = 1;
+    }
+  }
+
+  const c = Object.keys(map);
+
+  if (c.length === 1) {
+    if (map[k - Number(c[0])]) {
+      return map[k - Number(c[0])] - 1;
+    }
+  }
+
+  for (key in map) {
+    if (map[k - Number(key)]) {
+      console.log(key, map[k - Number(key)], map);
+      // ans.push(map[key],map[+key + +k])
+      ans++;
+    }
+  }
+};
+
+console.log(subarraySum());
